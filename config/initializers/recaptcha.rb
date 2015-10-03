b799f6b04fa89e5ec27e7c0579738fcc299b3412
ikukhar <ivkukhar@gmail.com>
2015-10-03 16:57:13 +0300

    Add recaptha keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6Lc6BAAAAAAAAChqRbQZcn_yyyyyyyyyyyyyyyyy'
-  config.private_key = '6Lc6BAAAAAAAAKN3DRm6VA_xxxxxxxxxxxxxxxxx'
+  config.public_key  = ENV['RECAPTCHA_KEY']
+  config.private_key = ENV['RECAPTCHA_SECRET_KEY']
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
   # Uncomment if you want to use the newer version of the API,
